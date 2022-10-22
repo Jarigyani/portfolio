52af5d7d5ea6fdb159e8e1eec0c1ef37943a89fd
Jarigyani <98578563+Jarigyani@users.noreply.github.com>
2022-10-22 09:30:21 +0000

    pixelate

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ const Home: NextPage = () => {
             </div>
             <div className="mr-5 flex-none">
               <img
-                className="w-10 rounded-full border-2 border border-green-500"
+                className="w-10 rounded-full border-2 border-green-500"
                 src="https://avatars.githubusercontent.com/u/98578563?s=400&u=557c7d92e931654fef9799f571cf0d9ba222e6ed&v=4"
                 alt="icon"
               />
