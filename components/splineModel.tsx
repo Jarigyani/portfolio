9c7e8142bb5bc7af305ffc5ec3b4e10bd51b0d30
Jarigyani <98578563+Jarigyani@users.noreply.github.com>
2022-10-24 05:21:36 +0000

    delete pixelanim

--- components/splineModel.tsx
+++ components/splineModel.tsx
@@ -4,16 +4,29 @@ import { useState } from 'react'
 
 export default function SplineModel() {
   const [visible, setVisible] = useState(false)
+
+  const onload = () => {
+    setVisible(true)
+  }
   return (
     <div className="h-full will-change-auto">
-      <motion.div
-        className={`h-full`}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3, delay: 1 }}
-      >
-        <Spline scene="https://prod.spline.design/umXHqIO9PqlFHs3L/scene.splinecode" />
-      </motion.div>
+      <div className="hidden">
+        <Spline
+          scene="https://prod.spline.design/umXHqIO9PqlFHs3L/scene.splinecode"
+          onLoad={onload}
+        />
+      </div>
+
+      {visible && (
+        <motion.div
+          className={`h-full`}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 3 }}
+        >
+          <Spline scene="https://prod.spline.design/umXHqIO9PqlFHs3L/scene.splinecode" />
+        </motion.div>
+      )}
     </div>
   )
 }
