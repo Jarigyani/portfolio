2897cb2325242a145c02b55fcfd1d40c1c705e5a
Jarigyani <98578563+Jarigyani@users.noreply.github.com>
2022-10-24 05:36:06 +0000

    fix performance

--- components/splineModel.tsx
+++ components/splineModel.tsx
@@ -1,32 +1,17 @@
 import Spline from '@splinetool/react-spline'
 import { motion } from 'framer-motion'
-import { useState } from 'react'
 
 export default function SplineModel() {
-  const [visible, setVisible] = useState(false)
-
-  const onload = () => {
-    setVisible(true)
-  }
   return (
     <div className="h-full will-change-auto">
-      <div className="hidden">
-        <Spline
-          scene="https://prod.spline.design/umXHqIO9PqlFHs3L/scene.splinecode"
-          onLoad={onload}
-        />
-      </div>
-
-      {visible && (
-        <motion.div
-          className={`h-full`}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 3 }}
-        >
-          <Spline scene="https://prod.spline.design/umXHqIO9PqlFHs3L/scene.splinecode" />
-        </motion.div>
-      )}
+      <motion.div
+        className={`h-full`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 1 }}
+      >
+        <Spline scene="https://prod.spline.design/umXHqIO9PqlFHs3L/scene.splinecode" />
+      </motion.div>
     </div>
   )
 }
