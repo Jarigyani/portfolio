656a74296b924c5f7814be0b8e246fdd433d8257
Jarigyani <coffee.mill.jp@gmail.com>
2022-12-14 23:10:46 +0900

    Fix: ブログ記事のボタンが押しにくい問題修正

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -8,20 +8,22 @@ type Props = {
 }
 const BlogCard = ({ src, alt, title, content }: Props) => {
   return (
-    <div key={alt} className="image-full card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img src={src} alt={alt} />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p>{content}</p>
-        <div className="card-actions justify-end">
-          <button className="btn-primary btn">
-            <Link href={`/blog/${alt}`}>Read Now</Link>
-          </button>
+    <Link href={`/blog/${alt}`}>
+      <div
+        key={alt}
+        className="image-full card h-60 w-96 bg-base-100 shadow-xl"
+      >
+        <figure>
+          <img src={src} alt={alt} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{content}</p>
+          {/* <div className="card-actions justify-end"> */}
+          <button className="btn-primary btn"> Read Now</button>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
